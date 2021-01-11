757e941fe258517cada32afe353f808e3b58d0e3
DANIEL PARK <danieljbpark1@gmail.com>
2021-01-11 13:10:47 -0500

    simulating longitudinal microbiome data.

---
+++ microbiome_sim.R
@@ -0,0 +1,7 @@
+## SIMULATING MICROBIOME COMPOSITION OVER TIME
+## start with t=1 vector of relative abundances
+## simulate fold-changes for one time step
+## simulate disappearances
+## simulate reappearances
+## append t+1 vector of rel. abnd.
+
